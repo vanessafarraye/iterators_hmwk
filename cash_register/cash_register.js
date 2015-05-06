1d7f19da98b67dc9ba4eaea7b5aa5ea3d8645ab0
nathanallen <thenathanator@gmail.com>
2015-05-06 12:51:42 -0700

    switcharoo

--- cash_register/cash_register.js
+++ cash_register/cash_register.js
@@ -1,10 +1,10 @@
 var line_items = [
-    {description: "aardvark", price: 425, qty: 1},
+    {description: "aardvark", price: 425, qty: -1},
     {description: "PruNe", price: 1.99, qty: 1},
     {description: "potato", price: .79, qty: -10},
     {description: "zebra", price: 525.25, qty: 1},
     {description: "SpinAch", price: 2.99, qty: 1},
-    {description: "zepplin", price: 20000, qty: -1},
+    {description: "zepplin", price: 20000, qty: 1},
     {description: "PetUnia", price: 1.25, qty: 12},
     {description: "squash", price: 2.35, qty: 3}
 ]
